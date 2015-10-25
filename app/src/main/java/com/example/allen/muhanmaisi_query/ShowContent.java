f1b15730ef1708e2a7de76d256f3aab6585658aa
boyxiaolong <boyhailong@gmai.com>
2015-10-25 23:02:57 +0800

    每一章按换显示

--- app/src/main/java/com/example/allen/muhanmaisi_query/ShowContent.java
+++ app/src/main/java/com/example/allen/muhanmaisi_query/ShowContent.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShowContent extends AppCompatActivity {
 
     private TextView textView;
+    private int curPage = 0;
+    private List<String> contentList = new ArrayList<>();
+
     private int getNormalBegin(int chapter){
         return (chapter-1)*25;
     }
@@ -19,6 +24,7 @@ public class ShowContent extends AppCompatActivity {
         setContentView(R.layout.activity_show_content);
 
         textView = (TextView)findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -32,15 +38,60 @@ public class ShowContent extends AppCompatActivity {
                     int chapter = Integer.parseInt(key);
                     int begin = getNormalBegin(chapter);
                     StringBuilder builder = new StringBuilder();
-                        for (int i = begin; i < begin+25 && i < chineseList.size(); ++i) {
-                            builder.append(arbicList.get(i) + "\n");
-                            builder.append(chineseList.get(i) + "\n");
-                        }
+                    for (int i = begin; i < begin+25 && i < chineseList.size(); ++i) {
+                        contentList.add(arbicList.get(i) + "\n" + chineseList.get(i) + "\n");
+                    }
 
-                    textView.setText(builder.toString());
-                    textView.setMovementMethod(new ScrollingMovementMethod());
+                    curPage = 1;
+                    showPageContent();
                 }
             }
         }
+    }
+
+    private void showPageContent(){
+        int begin = (curPage-1)*5;
+        int end = begin + 5;
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("第"+curPage+"换\n");
+        for (int i = begin; i < end && i < contentList.size(); ++i){
+            builder.append(contentList.get(i));
+        }
+
+        textView.setText(builder.toString());
+    }
+
+    private float x1,x2;
+    private final float Min_Distance = 150;
+
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                if (x2 - x1 > Min_Distance)
+                {
+                    if (curPage == 1) {
+                        break;
+                    }
+                    --curPage;
+                    showPageContent();
+                }
+                else if (x1 - x2 > Min_Distance)
+                {
+                    if (curPage == 5){
+                        break;
+                    }
+                    ++curPage;
+                    showPageContent();
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
